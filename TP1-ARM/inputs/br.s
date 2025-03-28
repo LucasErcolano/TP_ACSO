409da3c21a7850bbdb840747ed643f6635fc582b
Eliana Ostro <elianaostro@gmail.com>
2025-03-28 14:12:57 -0300

    Actualizar archivos de entrada con nuevas instrucciones y correcciones de formato

--- TP1-ARM/inputs/br.s
+++ TP1-ARM/inputs/br.s
@@ -1,9 +1,16 @@
 .text
-MOVZ X0, 0x400008
+ADD X0, X0, 0X4000
+LSL X0, X0, 4
+ADD X0, X0, 24
+BR X0
 B label1
 HLT 0
 
 label1:
-ADD X1, X2, 10
-BR X0
+ADD X1, X2, #10                 // Sumar 10 a X2 y almacenar en X1
+BR X0                           // Saltar a la dirección almacenada en X0
+ADD X2,X3,1
+ADD X3,X4,1
+ADD X4,X5,1
+ADD X5,X6,1
 HLT 0
--- TP1-ARM/inputs/ls.s
+++ TP1-ARM/inputs/ls.s
@@ -3,4 +3,5 @@ adds X3, X0, 12
 lsl X4, X3, 4
 adds X3, X0, 12
 lsr X4, X3, 4
+
 HLT 0
